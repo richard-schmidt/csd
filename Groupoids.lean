1eb0897b332447b949972221a48ebd0c681c8ffa
R. Schmidt <richard.schmidt.25@gmail.com>
2026-01-15 21:41:27 +0100

    Added SmallGroupoid Natural Transformation, with composition proof and some slightly nontrivial examples

--- Groupoids.lean
+++ Groupoids.lean
@@ -38,6 +38,69 @@ def sgNatComp
 {A : SmallGroupoid α} {B : SmallGroupoid β}
 {F G H : SGFun A B}
 (η₁ : SGNat F G) (η₂ : SGNat G H) : SGNat F H where
-  η x := B.comp (η₁.η x) (η₂.η x)
+  η := fun x => B.comp (η₁.η x) (η₂.η x)
   homEq {x y : A.ob} (f : A.hom x y) := by
-    sorry
+    rw [← B.assoc (F.homMap f) (η₁.η y) (η₂.η y)]
+    rw [η₁.homEq f]
+    rw [B.assoc (η₁.η x) (G.homMap f) (η₂.η y)]
+    rw [η₂.homEq f]
+    rw [B.assoc (η₁.η x) (η₂.η x) (H.homMap f)]
+
+
+class HomGrpd {α : Type u} {β : Type v} (A : SmallGroupoid α) (B : SmallGroupoid β) :
+Type ((max u v) + 3) where
+  hom : ∀ (_ _ : SGFun A B), (Type ((max u v) + 2))
+  comp : ∀ {F G H : SGFun A B}, hom F G -> hom G H -> hom F H
+  assoc : ∀ {F G H I : SGFun A B} (α : hom F G) (β : hom G H) (σ : hom H I),
+  comp (comp α β ) σ = comp α (comp β σ)
+  id : ∀ {F : SGFun A B}, hom F F
+  unitality : ∀ {F G : SGFun A B} (f : hom F G), comp f id = f ∧ comp id f = f
+
+
+def TwoObGpd : SmallGroupoid ℕ where
+  carrier := {1, 2}
+  hom x y := PLift (x = y)
+  comp f g := ⟨ Eq.trans f.down g.down ⟩
+  id _ := ⟨rfl⟩
+  inv f := ⟨f.down.symm⟩
+  assoc _ _ _ := rfl
+  unitality _ := ⟨rfl, rfl⟩
+  left_inv _ := rfl
+  right_inv _ := rfl
+
+def TwoIsoGpd : SmallGroupoid ℕ where
+  carrier := {1, 2}
+  hom _ _ := PLift (true)
+  comp f g := ⟨ Eq.trans f.down g.down ⟩
+  id _ := ⟨rfl⟩
+  inv f := ⟨f.down.symm⟩
+  assoc _ _ _ := rfl
+  unitality _ := ⟨rfl, rfl⟩
+  left_inv _ := rfl
+  right_inv _ := rfl
+
+def F : SGFun TwoObGpd TwoIsoGpd where
+ objectMap x := ⟨ 1, by simp ⟩
+ homMap _ := ⟨ rfl ⟩
+ compMap _ _ := rfl
+ idMap _ := rfl
+
+def G : SGFun TwoObGpd TwoIsoGpd where
+ objectMap x := ⟨ 2, by simp ⟩
+ homMap _ := ⟨ rfl ⟩
+ compMap _ _ := rfl
+ idMap _ := rfl
+
+def NatFG : SGNat F G where
+  η _ := ⟨ rfl ⟩
+  homEq _ := rfl
+
+instance : HomGrpd TwoObGpd TwoIsoGpd where
+  hom F G := SGNat F G
+  comp := sgNatComp
+  id := {η := fun _ => ⟨ rfl ⟩ , homEq := fun _ => rfl}
+  assoc := by intros; rfl
+  unitality := by
+    intros
+    apply And.intro rfl
+    exact rfl
